bd6ed12a680e6ba77a45742b448d3d7687f24496
GregoryPetrovLn <grishap96123@gmail.com>
2023-05-07 10:57:55 +0300

    created server file

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const connectDB = require("./config/db");
+
+require("colors");
+
+//Load env files
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config/.env" });
+
+const app = express();
+
+//Init server
+const PORT = process.env.PORT || 3300;
+const server = app.listen(PORT, () => {
+  console.log("==================================================");
+  console.log("==================================================");
+  console.log("==================================================");
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  );
+});
