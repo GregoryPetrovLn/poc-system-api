f6bd91e2181d0613578a487d4849c181892091ac
GregoryPetrovLn <grishap96123@gmail.com>
2023-05-07 12:05:14 +0300

    connetcted  middleware to server

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const security = require("./src/middleware/security");
 require("colors");
 
 //Load env files
@@ -11,8 +14,25 @@ const app = express();
 //Body parser
 app.use(express.json());
 
+//Cookie parser
+app.use(cookieParser());
+
+//Import routes
+const products = require("./src/routes/products");
+
 //DB
 connectDB();
+
+//Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+//Security middleware
+security(app);
+
+//Mount routes
+app.use("/api/products", products);
 
 //Init server
 const PORT = process.env.PORT || 3300;
@@ -23,4 +43,10 @@ const server = app.listen(PORT, () => {
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   );
+});
+
+//Handler unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  server.close(() => process.exit(1));
 });
