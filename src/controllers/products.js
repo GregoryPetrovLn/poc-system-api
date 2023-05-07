8368e11ff2302660bc1a73dffb56e8954b9f8280
GregoryPetrovLn <grishap96123@gmail.com>
2023-05-07 15:15:18 +0300

    fixed returned value

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -13,10 +13,9 @@ exports.getListProducts = asyncHandler(async (_, res) => {
 //@access Private
 exports.createProduct = asyncHandler(async (req, res, next) => {
   const product = await Product.create(req.body);
-
   res.status(200).json({
     success: true,
-    data: product,
+    data: product._id.toString(),
   });
 });
 
