8e4933e5d83c0cd18a5e56c6e99a808761344bf4
GregoryPetrovLn <grishap96123@gmail.com>
2023-05-07 15:03:52 +0300

    created post route

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,8 +1,21 @@
 const asyncHandler = require("../middleware/async");
+const Product = require("../models/Product");
 
 //@desc Get all products
 //@route GET /api/products
 //@access Public
 exports.getListProducts = asyncHandler(async (_, res) => {
   res.status(200).json(res.advancedResults);
+});
+
+//@desc Create new product
+//@route POST /api/product
+//@access Private
+exports.createProduct = asyncHandler(async (req, res, next) => {
+  const product = await Product.create(req.body);
+
+  res.status(200).json({
+    success: true,
+    data: product,
+  });
 });
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const advancedResults = require("../middleware/advancedResults");
-const { getListProducts } = require("../controllers/products");
+const { getListProducts, createProduct } = require("../controllers/products");
 const router = express.Router();
 //Protect route middleware
 const { protect, authorize } = require("../middleware/auth");
 const Product = require("../models/Product");
 
-//Protect for all routes
-// router.use(protect);
-// router.use(authorize("admin"));
+router
+  .route("/")
+  .get(advancedResults(Product), getListProducts)
+  .post(protect, authorize("admin"), createProduct);
 
-router.route("/").get(advancedResults(Product), getListProducts);
 
 module.exports = router;
